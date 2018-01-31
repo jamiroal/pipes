dc3bc17f0ca4eb46d62bd8b689b1114d68fb08c5
Jahan <jamiroal@gmail.com>
2018-01-31 12:17:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  nombre = "Yeral";
+  nombre2 = "Yeraldin DaYaNa MARIN pUERTA";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario=1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion:{
+      calle: "Primera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject) =>{
+    setTimeout(()=>resolve('Llego la data!'), 3500)
+  })
+
+  fechaTexto = '2017-01-01';
+  fecha = new Date();
+
+  video = "xKKeqlBQ3Js";
+
+  activar:boolean = true;
+
+
+
 }
